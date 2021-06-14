25a5c108a0b8e0b96dc022e54c3d61d79b2c1dae
Wujay Jung <wujay.jung@gmail.com>
2021-06-14 16:08:27 +0800

    Update guessing_game.py
    only 2 left to do:
    1. mention existing highscore when restarting
    2. congratulate if highscore beaten

--- guessing_game.py
+++ guessing_game.py
@@ -11,14 +11,11 @@ import random
 def start_game():
     """Psuedo-code Hints
     
-    1. Make sure no errors happen (non-number guesses)
-    2. Errors for numbers out of the range 
-    3. Add Restart option at end of game: would you like to play again?
-    - if no: show a closing message
-    - if yes: "Here we go again! Try to beat ____"
-    4. Show Current Score when playing again
-    ---> if high score gets beaten: congratulate
-    5. random number has to get updated everytime. 
+    1. mention existing high-score when restarting
+            "Here we go again! Try to beat ____"
+    2. if high score gets beaten, congratulate! 
+ 
+
 
     
     """
@@ -29,22 +26,36 @@ def start_game():
     lower_range = 1
     number = random.randrange(lower_range, upper_range, 1)
     guess = input("Guess a number between the range {} and {}  ".format(lower_range, upper_range))
-    trys = 1
-    guess = int(guess)
+    tries = 1
     while number != guess:
-      trys += 1
-      if number < guess:
-        print("It's lower")
-        guess = input("Try again: ")
+      tries += 1
+      try: 
         guess = int(guess)
-      elif number > guess:
-        print("It's higher")
+        if guess > 100 or guess < 1:
+          raise ValueError("You can only choose a number between {} and {}.".format(lower_range, upper_range))
+          guess = input("Try again: ")
+      except ValueError as err:
+        print("That is not a valid value. {}".format(err))
         guess = input("Try again: ")
-        guess = int(guess)
-      elif number == guess:
-        break
-    
-    print("Got it! It took you {} tries! ".format(trys))
+      else:
+        if number < guess:
+          print("It's lower")
+          guess = input("Try again: ")
+          guess = int(guess)
+        elif number > guess:
+          print("It's higher")
+          guess = input("Try again: ")
+          guess = int(guess)
+        elif number == guess:
+          break
+    print("Got it! It took you {} tries! ".format(tries))
+
+    restart = input("The game has finished. Would you like to start again? (yes/no) ")
+    if restart.lower() == "yes":
+      start_game()
+    else:
+      print("Thank you for playing. Come again. ")
+          
 
 
 
